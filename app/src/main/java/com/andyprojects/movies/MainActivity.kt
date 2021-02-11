4ebdb35265c1bc3e3c4577dba3c3220c71a7071f
Teslim <teslimakande@gmail.com>
2021-02-11 07:05:25 +0100

    fix top-level navigation

--- app/src/main/java/com/andyprojects/movies/MainActivity.kt
+++ app/src/main/java/com/andyprojects/movies/MainActivity.kt
@@ -5,27 +5,19 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.andyprojects.movies.databinding.ActivityMainBinding
-import com.andyprojects.movies.screens.discover.DiscoverFragment
-import com.andyprojects.movies.screens.genres.GenresFragment
-import com.andyprojects.movies.screens.movies.MoviesFragment
-import com.andyprojects.movies.screens.settings.SettingsFragment
-import com.andyprojects.movies.screens.tvShows.TvShowsFragment
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private const val KEY = "N"
-    }
 
     private lateinit var navDrawer: DrawerLayout
     private lateinit var navView: NavigationView
@@ -39,66 +31,57 @@ class MainActivity : AppCompatActivity() {
             .setContentView(this, R.layout.activity_main)
         binding.lifecycleOwner = this
 
-        appToolbar = binding.appToolbar
-        setSupportActionBar(appToolbar)
-
         navDrawer = binding.drawerLayout
         navView = binding.navView
+
+        val navController = this.findNavController(R.id.home_fragment_container)
+
+        appToolbar = binding.appToolbar
+        val topLevelDestination = mutableSetOf(
+            R.id.moviesFragment,
+            R.id.tvShowsFragment,
+            R.id.genresFragment,
+            R.id.discoverFragment
+        )
+        val appBarConfiguration = AppBarConfiguration(topLevelDestination, navDrawer)
+        setSupportActionBar(appToolbar)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
         navView.setNavigationItemSelectedListener {
+            val graph = navController.graph
             when (it.itemId) {
                 R.id.movies -> {
-                    switchHomeFragment(MoviesFragment(), it.itemId)
+                    graph.startDestination = R.id.moviesFragment
+                    navController.graph = graph
                 }
 
                 R.id.tvShows -> {
-                    switchHomeFragment(TvShowsFragment(), it.itemId)
+                    graph.startDestination = R.id.tvShowsFragment
+                    navController.graph = graph
                 }
 
                 R.id.genres -> {
-                    switchHomeFragment(GenresFragment(), it.itemId)
+                    graph.startDestination = R.id.genresFragment
+                    navController.graph = graph
                 }
 
                 R.id.discover -> {
-                    switchHomeFragment(DiscoverFragment(), it.itemId)
+                    graph.startDestination = R.id.discoverFragment
+                    navController.graph = graph
                 }
 
                 R.id.settings -> {
-                    switchHomeFragment(SettingsFragment(), it.itemId)
                 }
             }
             navDrawer.closeDrawer(GravityCompat.START)
             true
         }
-
-        val toggle = ActionBarDrawerToggle(
-            this, navDrawer, appToolbar,
-            R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        navDrawer.addDrawerListener(toggle)
-        toggle.syncState()
-
-        if (savedInstanceState == null) {
-            switchHomeFragment(MoviesFragment(), R.string.title_movies)
-            navView.setCheckedItem(R.id.movies)
-        } else {
-            navView.setCheckedItem(savedInstanceState.getInt(KEY))
-            navView.checkedItem?.let {
-                changeHomeTitle(it.itemId)
-            }
-        }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        navView.checkedItem?.let {
-            outState.putInt(KEY, it.itemId)
-        }
-        super.onSaveInstanceState(outState)
-    }
-
-    override fun onBackPressed() {
-        if (navDrawer.isDrawerOpen(GravityCompat.START))
-            navDrawer.closeDrawer(GravityCompat.START)
-        else super.onBackPressed()
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.home_fragment_container)
+        return NavigationUI.navigateUp(navController, navDrawer)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -124,27 +107,6 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    private fun switchHomeFragment(fragment: Fragment, itemId: Int) {
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction
-            .replace(R.id.home_fragment_container, fragment)
-            .commit()
-        changeHomeTitle(itemId)
-    }
-
-    private fun changeHomeTitle(itemId: Int) {
-        supportActionBar?.let {
-            title = when (itemId) {
-                R.id.movies -> getString(R.string.title_movies)
-                R.id.tvShows -> getString(R.string.title_tv_shows)
-                R.id.genres -> getString(R.string.title_genres)
-                R.id.discover -> getString(R.string.title_discover)
-                R.id.settings -> getString(R.string.title_settings)
-                else -> return
-            }
         }
     }
 }
